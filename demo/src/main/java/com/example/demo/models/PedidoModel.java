2ecfcb4d12f7be39c51f008bffc35d524adf5348
salomoa <salomaoantoniobr@gmail.com>
2026-03-16 22:14:24 -0300

    Inclusão do PedidoModel

--- demo/src/main/java/com/example/demo/models/PedidoModel.java
+++ demo/src/main/java/com/example/demo/models/PedidoModel.java
@@ -1,4 +1,38 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TB_PEDIDO")
 public class PedidoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime data;
+    private BigDecimal valorTotal;
+    private String status;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    @Column(name = "dataPedido")
+    public LocalDateTime getData() { return data; }
+
+
+    public void setData(LocalDateTime data) { this.data = data; }
+
+    @Column(name = "valorPedido")
+    public BigDecimal getValorTotal() { return valorTotal; }
+
+    public void setValorTotal(BigDecimal valorTotal) { this.valorTotal = valorTotal; }
+
+    @Column(name = "statusPedido")
+    public String getStatus() { return status; }
+
+    public void setStatus(String status) { this.status = status; }
 }
