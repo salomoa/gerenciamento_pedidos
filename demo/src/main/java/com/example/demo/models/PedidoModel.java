aca70bba700d046c558d882ec567c46f72a263c6
salomoa <salomaoantoniobr@gmail.com>
2026-03-24 20:15:40 -0300

    Refactor PedidoModel to include column annotations

--- demo/src/main/java/com/example/demo/models/PedidoModel.java
+++ demo/src/main/java/com/example/demo/models/PedidoModel.java
@@ -12,26 +12,25 @@ public class PedidoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "dataPedido")
     private LocalDateTime data;
+    @Column(name = "valorPedido")
     private BigDecimal valorTotal;
+    @Column(name = "statusPedido")
     private String status;
 
     public Long getId() { return id; }
 
     public void setId(Long id) { this.id = id; }
 
-    @Column(name = "dataPedido")
     public LocalDateTime getData() { return data; }
-
 
     public void setData(LocalDateTime data) { this.data = LocalDateTime.now(); }
 
-    @Column(name = "valorPedido")
     public BigDecimal getValorTotal() { return valorTotal; }
 
     public void setValorTotal(BigDecimal valorTotal) { this.valorTotal = valorTotal; }
-
-    @Column(name = "statusPedido")
+    
     public String getStatus() { return status; }
 
     public void setStatus(String status) { this.status = status; }
